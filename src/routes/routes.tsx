93b815ec3c9d903a79bb657cc8b9ce8a4947158c
dev-talukdar <khejurbd@gmail.com>
2024-02-27 12:59:19 +0600

    dashboard is now dynamic

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { adminPaths } from "./AdminRoutes";
 import routeGenerator from "../utils/routeGenerator";
+import { facultyPaths } from "./FacultyRoutes";
+import { studentPaths } from "./StudentRoutes";
 
 const router = createBrowserRouter([
   {
@@ -16,12 +18,12 @@ const router = createBrowserRouter([
   {
     path: "/faculty",
     element: <App></App>,
-    children: routeGenerator(adminPaths),
+    children: routeGenerator(facultyPaths),
   },
   {
     path: "/student",
     element: <App></App>,
-    children: routeGenerator(adminPaths),
+    children: routeGenerator(studentPaths),
   },
 ]);
 
