8852533fd50d7d4a3a5fc9459ece9fab6f6da61e
dev-talukdar <khejurbd@gmail.com>
2024-03-17 14:57:38 +0600

    style added

--- src/redux/features/student/Course/StudentOfferedCourse.tsx
+++ src/redux/features/student/Course/StudentOfferedCourse.tsx
@@ -1,5 +1,5 @@
 import { useGetAllOfferedCourseQuery } from "../../admin/studentCourseApi";
-import { Button, Row } from "antd";
+import { Button, Col, Row } from "antd";
 
 const StudentOfferedCourse = () => {
   const { data: offeredCourseData } = useGetAllOfferedCourseQuery(undefined);
@@ -21,30 +21,37 @@ const StudentOfferedCourse = () => {
 
   const modifiedData = Object.values(singleObject ? singleObject : {});
   return (
-    <Row>
+    <Row gutter={[0, 20]}>
       {modifiedData.map((item) => {
         return (
-          <div>
+          <Col
+            span={24}
+            style={{ border: "solid #d4d4d4 2px", padding: "10px" }}
+          >
             <h2>Course Name: {item.courseTitle}</h2>
             <div>
               {item.sections.map((section) => {
                 return (
-                  <div>
-                    <p>Section: {section.section}</p>
-                    <div>
+                  <Row
+                    justify="space-between"
+                    align="middle"
+                    style={{ borderTop: "solid #d4d4d4 2px" }}
+                  >
+                    <Col span={5}>Section: {section.section}</Col>
+                    <Col span={5}>
                       Days:{" "}
                       {section.days.map((day) => (
                         <span>{day}</span>
                       ))}
-                    </div>
-                    <p>Class Started: {section.startTime}</p>
-                    <p>Class Ended: {section.endTime}</p>
+                    </Col>
+                    <Col span={5}>Class Started: {section.startTime}</Col>
+                    <Col span={5}>Class Ended: {section.endTime}</Col>
                     <Button>Enroll Now</Button>
-                  </div>
+                  </Row>
                 );
               })}
             </div>
-          </div>
+          </Col>
         );
       })}
     </Row>
